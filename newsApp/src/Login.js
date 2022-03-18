932d770e7a8f32e80c0f6497b1f7786a564b562a
kamilsmr <kamils6035@gmail.com>
2022-03-19 00:04:05 +0300

    login

---
+++ newsApp/src/Login.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {SafeAreaView, View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+
+const Login = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Image
+            source={require('./assets/icon.png')}
+            style={styles.logoStyle}
+          
+        />
+        <Text>Login</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
+
+
+const styles = StyleSheet.create({
+    logoStyle:{
+        backgroundColor:'red',
+        resizeMode:'contain',
+        width:Dimensions.get('window').width,
+        height:800,
+        
+    }
+
+})
+
